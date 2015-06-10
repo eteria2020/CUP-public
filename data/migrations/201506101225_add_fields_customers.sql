30772b369adb88f3a3b4413f0e1ca44fbc6f775f
vikey89 <vincenzo.provenza89@gmail.com>
2015-06-10 14:27:10 +0200

    set fields to not null

--- data/migrations/201506101225_add_fields_customers.sql
+++ data/migrations/201506101225_add_fields_customers.sql
@@ -1,2 +1,2 @@
-ALTER TABLE customers ADD gold_list BOOLEAN DEFAULT false;
-ALTER TABLE customers ADD maintainer BOOLEAN DEFAULT false;
+ALTER TABLE customers ADD gold_list BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE customers ADD maintainer BOOLEAN NOT NULL DEFAULT false;
