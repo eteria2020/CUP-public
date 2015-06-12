e7b155931c1c795def16106718828b6623a4b6fd
Marco Perone <m.perone@mvlabs.it>
2015-06-12 13:06:54 +0200

    other configuration parameters

--- config/autoload/cartasi.global.php
+++ config/autoload/cartasi.global.php
@@ -4,6 +4,9 @@ return [
     'cartasi' => [
         'first_payment_amount' => 1000,
         'currency' => 'EUR',
-        'first_payment_url' => 'https://ecommerce.keyclient.it/ecomm/ecomm/DispatcherServlet'
+        'first_payment_url' => 'https://ecommerce.keyclient.it/ecomm/ecomm/DispatcherServlet',
+        'first_payment_description' => '',
+        'recurring_payment_url' => 'https://ecommerce.keyclient.it/ecomm/ecomm/ServletS2S',
+        'recurring_payment_description' => ''
     ]
 ];
