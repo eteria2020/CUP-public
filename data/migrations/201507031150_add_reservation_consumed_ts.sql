5a3cf7000a33e06ba419d3316d142c85cce4d692
Francesco De Marchi <demarchi@devotech.it>
2015-07-03 17:18:33 +0200

    Chenged consumed_ts type to proper timestamp(0) with timezone

--- data/migrations/201507031150_add_reservation_consumed_ts.sql
+++ data/migrations/201507031150_add_reservation_consumed_ts.sql
@@ -1,1 +1,17 @@
-ALTER TABLE reservations ADD consumed_ts timestamp with time zone;
+DROP VIEW "Prenotazioni ALARMS";
+
+ALTER TABLE reservations ALTER consumed_ts TYPE timestamp(0) with time zone;
+
+CREATE OR REPLACE VIEW "Prenotazioni ALARMS" AS
+SELECT reservations.id,
+reservations.ts,
+reservations.car_plate,
+reservations.customer_id,
+reservations.beginning_ts,
+reservations.active,
+reservations.length,
+reservations.to_send,
+reservations.sent_ts,
+reservations.consumed_ts
+FROM reservations
+WHERE reservations.active IS TRUE AND reservations.length = (-1);
