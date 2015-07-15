7cf115b0057357949f56ce0480ef93e0dd425101
Francesco De Marchi <demarchi@devotech.it>
2015-07-15 09:26:13 +0200

    Added resent_ts

--- data/migrations/201507150900_update_reservations.sql
+++ data/migrations/201507150900_update_reservations.sql
@@ -1,1 +1,4 @@
 ALTER TYPE reservations_archive_reason ADD VALUE 'ALARM-OFF';
+
+ALTER TABLE reservations ADD resent_ts timestamp(0) with time zone;
+ALTER TABLE reservations_archive ADD resent_ts timestamp(0) with time zone;
