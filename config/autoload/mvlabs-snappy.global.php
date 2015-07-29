8ffddbf393f285a0e19c310bc5d5e0ad022f3937
Stefano Valle <s.valle@mvassociati.it>
2015-07-29 13:05:28 +0200

    Add wkhtmltopdf dependency

---
+++ config/autoload/mvlabs-snappy.global.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'mvlabs-snappy' => array(
+        'pdf' => array(
+           'binary'  => '/srv/apps/sharengo-publicsite/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64',
+           'options' => array(),
+        )
+     )   
+);
