c57a8c77e88da6dcc34645f515e6432cb361ff40
Francesco De Marchi <demarchi@devotech.it>
2015-08-05 15:43:57 +0200

    Added AssetManager module to config

--- config/application.config.php
+++ config/application.config.php
@@ -14,6 +14,7 @@ return array(
         'GoalioForgotPassword',
         'GoalioForgotPasswordDoctrineORM',
         'MobileDetectModule',
+        'AssetManager',
         'MvlabsSnappy',
 
         // Our modules
