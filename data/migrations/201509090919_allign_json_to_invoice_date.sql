051a1c441ff39c2da65ab55e4eb3aa455f2f61a1
Francesco De Marchi <f.demarchi@mvlabs.it>
2015-09-09 09:43:34 +0200

    regex fix

--- data/migrations/201509090919_allign_json_to_invoice_date.sql
+++ data/migrations/201509090919_allign_json_to_invoice_date.sql
@@ -1,1 +1,1 @@
-UPDATE invoices SET content = regexp_replace(content::text, '"invoice_date":[0-9]+', '"invoice_date":' || invoice_date || '')::jsonb;
+UPDATE invoices SET content = regexp_replace(content::text, '"invoice_date": [0-9]+', '"invoice_date": ' || invoice_date || '')::jsonb;
