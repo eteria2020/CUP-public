0b6ffd7311067463c9b64dbaba418cadb281929b
Francesco De Marchi <f.demarchi@mvlabs.it>
2015-09-15 15:06:05 +0200

    Added popup

---
+++ public/js/bonus-packages.js
@@ -0,0 +1,38 @@
+var isPopupVisible = false;
+document.getElementById("close-btn").addEventListener('click', function (event)
+{
+    close();
+});
+
+function buy(packageId)
+{
+    // get the cars
+    $.get(packageUrl + packageId, function (jsonData)
+    {
+        document.getElementById("description").innerHTML = jsonData.data.description;
+        document.getElementById("minutes").innerHTML = jsonData.data.minutes;
+        document.getElementById("cost").innerHTML = parseCost(jsonData.data.cost);
+        document.getElementById("valid-from").innerHTML = jsonData.data.validFrom;
+        document.getElementById("valid-to").innerHTML = jsonData.data.validTo;
+        document.getElementById("available-until").innerHTML = jsonData.data.buyableUntil;
+    });
+
+    togglePopup();
+}
+
+function close()
+{
+    togglePopup();
+}
+
+function togglePopup()
+{
+    document.getElementById("pack-popup-container").style.display =
+        isPopupVisible ? 'none' : 'inline';
+    isPopupVisible = !isPopupVisible;
+}
+
+function parseCost(cost)
+{
+    return Math.floor(cost / 100) + ',' + cost % 100 + (cost % 100 < 10 ? '0' : '') + '\u20ac';
+}
