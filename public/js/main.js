6a70c9cd914797ee53a5b685d407cb5ee3b87a4f
Francesco De Marchi <f.demarchi@mvlabs.it>
2015-09-28 15:49:55 +0200

    Started implementing

--- public/js/main.js
+++ public/js/main.js
@@ -1,65 +1,58 @@
 $(window).load(function(){
 
 // add height main content
-function resize() {
-	  //var h = $(window).height() - $('#header').height() - $('#footer').height();
-	  // var h = $(window).height() - $('#header').height();  
-      //  $('.detect-window-height').height(h);
-	  
-	  //var hmap = $(window).height() - $('#header').height() - $('#footer').height() - 20;
-	   var hmap = $(window).height() - $('#header').height() - 20;  
-      $('.detect-map-height').height(hmap);
-  }
-  
-  $(window).resize(function () {
-	  resize();
+function resize()
+{
+    var hmap = $(window).height() - $('#header').height() - 20;
+    $('.detect-map-height').height(hmap);
+}
+
+$(window).resize(function () {
+	resize();
 });
-  
+
 resize();
- 
+
 
 // show column bar
-
 $('.js-toogle-column-app').on('click', function(e) {
-	  $('.module-wrapper-app').toggleClass("hide-side-bar");
-      $('#outer').toggleClass("hide-side-bar");
-	  $(this).toggleClass("side-close");     	 
-      e.preventDefault();
-    }); 
-    
-// add class on touch
+	$('.module-wrapper-app').toggleClass("hide-side-bar");
+    $('#outer').toggleClass("hide-side-bar");
+	$(this).toggleClass("side-close");
+    e.preventDefault();
+});
 
-$('.block-data-table-row-group').on('touchend', function(e) {  
+// add class on touch
+$('.block-data-table-row-group').on('touchend', function(e) {
 	'use strict';
 	var link = $(this);
 	if (link.hasClass('over')) {
-	return true;
+	   return true;
 	} else {
-	link.addClass('over');
-	$('.block-data-table-row-group').not(this).removeClass('over');
-	e.preventDefault();
-	return false; //extra, and to make sure the function has consistent return points
-}
-
-});    
+	   link.addClass('over');
+	   $('.block-data-table-row-group').not(this).removeClass('over');
+	   e.preventDefault();
+	   return false; //extra, and to make sure the function has consistent return points
+    }
+});
 
 // PLACEOLDER
 //Assign to those input elements that have 'placeholder' attribute
 if($.browser.msie){
-   $('input[placeholder]').each(function(){ 
-       
-        var input = $(this);       
-       
+   $('input[placeholder]').each(function(){
+
+        var input = $(this);
+
         $(input).val(input.attr('placeholder'));
-               
+
         $(input).focus(function(){
              if (input.val() == input.attr('placeholder')) {
                  input.val('');
              }
         });
-       
+
         $(input).blur(function(){
-            if (input.val() == '' || input.val() == input.attr('placeholder')) {
+            if (input.val() === '' || input.val() == input.attr('placeholder')) {
                 input.val(input.attr('placeholder'));
             }
         });
@@ -67,23 +60,27 @@ if($.browser.msie){
 };
 
 // SHOW BOX
-	
+
 $(".js-collapse-box").hide();
 $(".js-collapse-box").removeClass( "hidden" );
 $(".js-show-element").click(function(){
 	$(this).next().slideToggle('fast', function(){
 		$(this).prev(".js-show-element").toggleClass("active");
-		});
+	});
 	return false;
 });
-    
-// TOGGLE MAP HEIGHT 
-    
- $('.js-toggle-map-height').on('click', function(e) {
-  $('.module-car-map').toggleClass("small-height");
-  $(this).toggleClass("active");
-  e.preventDefault();
-});   
 
+// TOGGLE MAP HEIGHT
+
+$('.js-toggle-map-height').on('click', function(e) {
+    $('.module-car-map').toggleClass("small-height");
+    $(this).toggleClass("active");
+    e.preventDefault();
+});
+
+function cityPicker(code)
+{
+  console.log(code);
+}
 
 });
