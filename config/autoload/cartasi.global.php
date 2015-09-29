ebb0a9cd84c68db753a6f44857cba0e5c3c61ca9
Marco Perone <m.perone@mvlabs.it>
2015-09-29 10:37:11 +0200

    avoid trying other payments if one fails

--- config/autoload/cartasi.global.php
+++ config/autoload/cartasi.global.php
@@ -7,6 +7,8 @@ return [
         'first_payment_url' => 'https://ecommerce.keyclient.it/ecomm/ecomm/DispatcherServlet',
         'first_payment_description' => '',
         'recurring_payment_url' => 'https://ecommerce.keyclient.it/ecomm/ecomm/ServletS2S',
-        'recurring_payment_description' => ''
+        'recurring_payment_description' => '',
+        'number_of_retries' => 3,
+        'pause_between_retries' => 1
     ]
 ];
