f004d652b6b6478593d0c66d3c74b32175ebe941
Marco Perone <m.perone@mvlabs.it>
2015-10-01 20:16:51 +0200

    create new promo codes info

--- data/migrations/201510011905_insert_promo_codes.sql
+++ data/migrations/201510011905_insert_promo_codes.sql
@@ -1,14 +1,18 @@
+INSERT INTO promo_codes_info (id, active, insert_ts, type, valid_from, valid_to, overridden_subscription_cost) VALUES
+(nextval('promocodesinfo_id_seq'::regclass),true, '2015-10-02 00:00:00', 'promo', '2015-10-02 00:00:00', '2016-10-02 00:00:00', 100)
+
+
 INSERT INTO promo_codes Values(
-    nextval('promocode_id_seq'::regclass),
-    2,
+    nextval('promocodes_id_seq'::regclass),
+    , # usare l'id del promo_codes_info appena creato
     'BKGO',
     'Iscrizione ad 1 euro per soci BikeMi',
     true
 );
 
 INSERT INTO promo_codes Values(
-    nextval('promocode_id_seq'::regclass),
-    2,
+    nextval('promocodes_id_seq'::regclass),
+    , # usare l'id del promo_codes_info appena creato
     'ELFO',
     'Iscrizione ad 1 euro per soci Teatro dell''Elfo Puccini',
     true
