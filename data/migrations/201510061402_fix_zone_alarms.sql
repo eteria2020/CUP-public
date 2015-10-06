775531e0511b37fb91f3be9af58d4299ee47ec02
Francesco De Marchi <f.demarchi@mvlabs.it>
2015-10-06 14:26:05 +0200

    Fixed zoneAlarms for use with fleets

---
+++ data/migrations/201510061402_fix_zone_alarms.sql
@@ -0,0 +1,16 @@
+/**
+ * Add column to properly reference fleets
+ */
+ALTER TABLE zone_alarms ADD fleet_id integer REFERENCES fleets (id);
+
+/**
+ * Check whether the id's used here correspond to the ones in production
+ */
+UPDATE zone_alarms SET fleet_id = 1 WHERE name = 'MI';
+UPDATE zone_alarms SET fleet_id = 2 WHERE name = 'FI';
+ALTER TABLE zone_alarms ALTER COLUMN fleet_id SET NOT NULL;
+
+/**
+ * Remove the obsolete column
+ */
+ALTER TABLE zone_alarms DROP COLUMN name;
