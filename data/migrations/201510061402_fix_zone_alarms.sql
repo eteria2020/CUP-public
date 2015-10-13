2fc117fcdde5b55f50a05a9c6b61dad6466dd0c9
Marco Perone <m.perone@mvlabs.it>
2015-10-13 19:18:03 +0200

    small fixes

--- data/migrations/201510061402_fix_zone_alarms.sql
+++ data/migrations/201510061402_fix_zone_alarms.sql
@@ -12,9 +12,9 @@ ALTER TABLE zone_alarms ADD active boolean DEFAULT true NOT NULL;
  * Create table for many-to-many relationship
  */
 CREATE TABLE zone_alarms_fleets (
-    zone_alarm_id int REFERENCES zone_alarms (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    zone_alarm_id int REFERENCES zone_alarms (id) ON UPDATE CASCADE,
     fleet_id int REFERENCES fleets (id) ON UPDATE CASCADE,
-    CONSTRAINT zone_alarms_pkey PRIMARY KEY (bill_id, product_id)
+    CONSTRAINT "zone_alarms_fleets_pkey" PRIMARY KEY (zone_alarm_id, fleet_id)
 );
 
 /**
