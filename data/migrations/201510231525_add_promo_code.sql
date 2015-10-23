023e402f2a440c96b2e0f396dde12174bfe991c5
Francesco De Marchi <f.demarchi@mvlabs.it>
2015-10-23 15:35:23 +0200

    Added Linear landing page and promoCode

---
+++ data/migrations/201510231525_add_promo_code.sql
@@ -0,0 +1,10 @@
+/**
+ * Verify that 17 is the actual next id to be inserted
+ */
+INSERT INTO promo_codes Values(
+    17,
+    5,
+    'LNGO',
+    'Iscrizione ad 1 euro per soci Linear',
+    true
+);
