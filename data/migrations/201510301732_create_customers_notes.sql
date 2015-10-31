b043726565b09df0c165105390edd4b2799d8a58
Francesco De Marchi <f.demarchi@mvlabs.it>
2015-10-31 15:46:10 +0100

    Added migration

---
+++ data/migrations/201510301732_create_customers_notes.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customers_notes (
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers (id) NOT NULL,
+    webuser_id INT REFERENCES webuser (id) NOT NULL,
+    inserted_ts TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    note TEXT NOT NULL
+);
