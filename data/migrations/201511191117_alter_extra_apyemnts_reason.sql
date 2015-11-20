69f3cb03fd9a5823d7388e0b1173e865829de94b
Francesco De Marchi <f.demarchi@mvlabs.it>
2015-11-20 12:09:06 +0100

    Added enum

--- data/migrations/201511191117_alter_extra_apyemnts_reason.sql
+++ data/migrations/201511191117_alter_extra_apyemnts_reason.sql
@@ -1,11 +1,26 @@
 ALTER TABLE extra_payments
 ADD COLUMN reasons jsonb;
 
+CREATE TYPE extra_payments_types AS ENUM('extra', 'penalty');
+
+ALTER TABLE extra_payments
+ADD COLUMN types_temp extra_payments_types;
+
 UPDATE extra_payments
-SET reasons = ('{"' || reason || '": ' || amount::text || '}')::jsonb;
+SET reasons = ('{"' || reason || '": ' || amount::text || '}')::jsonb,
+    types_temp = payment_type::extra_payments_types;
 
 ALTER TABLE extra_payments
 ALTER COLUMN reasons SET NOT NULL;
 
 ALTER TABLE extra_payments
+ALTER COLUMN types_temp SET NOT NULL;
+
+ALTER TABLE extra_payments
 DROP COLUMN reason;
+
+ALTER TABLE extra_payments
+DROP COLUMN payment_type;
+
+ALTER TABLE extra_payments
+RENAME COLUMN types_temp TO payment_type;
