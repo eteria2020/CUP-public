65b813510e44b8fe6bfd71f7772d13c8428ddef4
Francesco De Marchi <f.demarchi@mvlabs.it>
2015-11-20 11:19:07 +0100

    Renamed to reasons from reason

--- data/migrations/201511191117_alter_extra_apyemnts_reason.sql
+++ data/migrations/201511191117_alter_extra_apyemnts_reason.sql
@@ -1,14 +1,11 @@
 ALTER TABLE extra_payments
-ADD COLUMN reason_temp jsonb;
+ADD COLUMN reasons jsonb;
 
 UPDATE extra_payments
-SET reason_temp = ('{"' || reason || '": ' || amount::text || '}')::jsonb;
+SET reasons = ('{"' || reason || '": ' || amount::text || '}')::jsonb;
 
 ALTER TABLE extra_payments
-ALTER COLUMN reason_temp SET NOT NULL;
+ALTER COLUMN reasons SET NOT NULL;
 
 ALTER TABLE extra_payments
 DROP COLUMN reason;
-
-ALTER TABLE extra_payments
-RENAME COLUMN reason_temp TO reason;
