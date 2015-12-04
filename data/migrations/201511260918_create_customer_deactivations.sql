793d00dd0a9b43c6a46280a7c70cc4e708bf0178
Francesco De Marchi <f.demarchi@mvlabs.it>
2015-12-04 11:29:52 +0100

    Fixed typo, got more info on failed payments

--- data/migrations/201511260918_create_customer_deactivations.sql
+++ data/migrations/201511260918_create_customer_deactivations.sql
@@ -1,7 +1,7 @@
 CREATE TYPE disabled_reason AS ENUM (
     'FIRST_PAYMENT_NOT_COMPLETED',
     'FAILED_PAYMENT',
-    'INVALD_DRIVERS_LICENSE',
+    'INVALID_DRIVERS_LICENSE',
     'DISABLED_BY_WEBUSER'
 );
 
@@ -29,12 +29,38 @@ SELECT nextval('customer_deactivations_id_seq'),
     NULL,
     NULL,
     NULL,
-    '{"deactivation":{"trip_payment_try_id":"","note":"Generated automatically"}}'
+    ('{"deactivation":{"trip_payment_try_id":"' || COALESCE(max(tpt.id)::text, '') || '","note":"Generated automatically"}}')::jsonb
 FROM customers c
+LEFT JOIN trips t ON c.id = t.customer_id
+LEFT JOIN trip_payments tp ON t.id = tp.trip_id
+LEFT JOIN trip_payment_tries tpt ON tp.id = tpt.trip_payment_id
 WHERE c.enabled = false
 AND c.payment_able = false
 AND c.first_payment_completed = true
-AND c.maintainer = false;
+AND c.maintainer = false
+AND (tpt.outcome = 'KO' OR tpt IS NULL)
+GROUP BY c.id;
+
+INSERT INTO customer_deactivations
+SELECT nextval('customer_deactivations_id_seq'),
+    now(),
+    c.id,
+    'FAILED_PAYMENT',
+    now(),
+    NULL,
+    NULL,
+    NULL,
+    '{"deactivation":{"trip_payment_try_id":"","note":"Generated automatically"}}'
+FROM customers c
+LEFT JOIN trips t ON c.id = t.customer_id
+LEFT JOIN trip_payments tp ON t.id = tp.trip_id
+LEFT JOIN trip_payment_tries tpt ON tp.id = tpt.trip_payment_id
+WHERE c.enabled = false
+AND c.payment_able = false
+AND c.first_payment_completed = true
+AND c.maintainer = false
+AND (tpt.outcome = 'KO' OR tpt IS NULL)
+GROUP BY c.id;
 
 /**
  * Customers disabled by webusers
