ce5794216500e9a42db076314b323b6b11ce7213
Francesco De Marchi <f.demarchi@mvlabs.it>
2015-12-10 16:48:03 +0100

    Updated facebook script

--- public/js/fbds.js
+++ public/js/fbds.js
@@ -1,14 +1,8 @@
-(function() {
-    var _fbq = window._fbq || (window._fbq = []);
-    if (!_fbq.loaded) {
-        var fbds = document.createElement('script');
-        fbds.async = true;
-        fbds.src = '//connect.facebook.net/en_US/fbds.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(fbds, s);
-        _fbq.loaded = true;
-    }
-})();
-window._fbq = window._fbq || [];
-window._fbq.push(['track', '6041748518720', {'value':'0.00','currency':'EUR'}]);
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','//connect.facebook.net/en_US/fbevents.js');
 
+fbq('init', '423565347840059');
+fbq('track', "PageView");
