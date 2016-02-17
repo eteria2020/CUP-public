8b582f2e7caae8dff555b4e50a9d6bbb347285a2
Massimo De Luisa <massimodeluisa@me.com>
2016-02-17 12:13:29 +0100

    1361 Code linted, moved banner ajax call from index.phtml to banner.js

--- public/js/banner.js
+++ public/js/banner.js
@@ -1,24 +1,43 @@
-//Predefined callback function    
-function setBanner(data) {
-    if (typeof data.banner === 'undefined'){
+/* global pageLink, userId, bannerJsonpUrl */
+
+/**
+ * Predefined callback function
+ */
+var setBanner = function(data) {
+    if (typeof data.banner === "undefined") {
         return false;
     }
 
-    $.each(data.banner, function(key,banner){
-        if (typeof banner.id === 'undefined'){
+    $.each(data.banner, function (key, banner) {
+        if (typeof banner.id === "undefined") {
             return false;
         }
 
-        if (typeof banner.html !== 'undefined'){
-            $("#"+banner.id).html(banner.html);
+        if (typeof banner.html !== "undefined") {
+            $("#" + banner.id).html(banner.html);
         }
 
-        if (typeof banner.img !== 'undefined'){
-            $("#"+banner.id+" img").prop("src",banner.img);
+        if (typeof banner.img !== "undefined") {
+            $("#" + banner.id + " img").prop("src", banner.img);
         }
 
-        if (typeof banner.link !== 'undefined'){
-            $("#"+banner.id+" a").prop("href",banner.link);
-        } 
+        if (typeof banner.link !== "undefined") {
+            $("#" + banner.id + " a").prop("href", banner.link);
+        }
     });
-}
+};
+
+// Load Banner
+$.ajax({
+    url: bannerJsonpUrl,
+    dataType: "jsonp",
+    type: "POST",
+    async: true,
+    data: {
+        id: userId,
+        link: pageLink,
+        callback: "setBanner"
+    },
+    crossDomain: true,
+    success: setBanner
+});
