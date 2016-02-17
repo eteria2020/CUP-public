eaeb135abc6998d1a5c8fc2fdfdd38b9b879a12c
Massimo De Luisa <massimodeluisa@me.com>
2016-02-17 15:01:13 +0100

    1361 Small fixes

--- public/js/banner.js
+++ public/js/banner.js
@@ -1,9 +1,9 @@
-/* global pageLink, userId, bannerJsonpUrl */
+/* global setBanner, loadBanner */
 
 /**
  * Predefined callback function
  */
-var setBanner = function(data) {
+var setBanner = function (data) {
     if (typeof data.banner === "undefined") {
         return false;
     }
@@ -27,17 +27,28 @@ var setBanner = function(data) {
     });
 };
 
-// Load Banner
-$.ajax({
-    url: bannerJsonpUrl,
-    dataType: "jsonp",
-    type: "POST",
-    async: true,
-    data: {
-        id: userId,
-        link: pageLink,
-        callback: "setBanner"
-    },
-    crossDomain: true,
-    success: setBanner
-});
+/**
+ * This function call a ajax to a bannerJsonpUrl that will return
+ * a JSONp containing the data needed by the setBanner function to
+ * compose the page banners.
+ *
+ *  @param  bannerJsonpUrl  The URL that return the JSONp
+ *  @param  userId  The id of the user
+ *  @param  pageLink  The link of the page that is calling this function
+ */
+var loadBanner = function (bannerJsonpUrl, userId, pageLink) {
+    // Load Banner
+    $.ajax({
+        url: bannerJsonpUrl,
+        dataType: "jsonp",
+        type: "POST",
+        async: true,
+        data: {
+            id: userId,
+            link: pageLink,
+            callback: "setBanner"
+        },
+        crossDomain: true,
+        success: setBanner
+    });
+};
