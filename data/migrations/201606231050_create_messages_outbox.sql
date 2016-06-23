c5d81680dea9067bbe5cfe085adef1246208d5e5
Massimo De Luisa <massimo.deluisa@bulweria.com>
2016-06-23 13:35:45 +0200

    migrations: Fixed 201606231050_create_messages_outbox.sql table creation order.

--- data/migrations/201606231050_create_messages_outbox.sql
+++ data/migrations/201606231050_create_messages_outbox.sql
@@ -1,3 +1,28 @@
+CREATE TABLE messages_transports
+(
+  name text NOT NULL,
+  description text,
+  CONSTRAINT messages_transports_pk PRIMARY KEY (name)
+);
+
+ALTER TABLE messages_transports OWNER TO sharengo;
+
+CREATE TABLE messages_types
+(
+  name text NOT NULL,
+  description text,
+  default_transport text,
+  CONSTRAINT messages_types_pk PRIMARY KEY (name),
+  CONSTRAINT default_transport_fk FOREIGN KEY (default_transport)
+      REFERENCES messages_transports (name) MATCH FULL
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+ALTER TABLE messages_types OWNER TO sharengo;
+
+COMMENT ON COLUMN messages_types.default_transport IS 'The default transport to use to send this message type.
+Can be overrided by any "messages_transports" records, specifing the transport in the homonym field.';
+
 CREATE TABLE messages_outbox
 (
   id serial NOT NULL,
@@ -18,34 +43,9 @@ CREATE TABLE messages_outbox
   CONSTRAINT type_fk FOREIGN KEY (type)
       REFERENCES messages_types (name) MATCH FULL
       ON UPDATE NO ACTION ON DELETE NO ACTION
-)
+);
 
 ALTER TABLE messages_outbox OWNER TO sharengo;
 
 COMMENT ON COLUMN messages_outbox.sent_meta IS 'This field contains send information in JSON type.
 Shoud countain for example send errors. ';
-
-CREATE TABLE messages_transports
-(
-  name text NOT NULL,
-  description text,
-  CONSTRAINT messages_transports_pk PRIMARY KEY (name)
-)
-
-ALTER TABLE messages_transports OWNER TO sharengo;
-
-CREATE TABLE messages_types
-(
-  name text NOT NULL,
-  description text,
-  default_transport text,
-  CONSTRAINT messages_types_pk PRIMARY KEY (name),
-  CONSTRAINT default_transport_fk FOREIGN KEY (default_transport)
-      REFERENCES messages_transports (name) MATCH FULL
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-
-ALTER TABLE messages_types OWNER TO sharengo;
-
-COMMENT ON COLUMN messages_types.default_transport IS 'The default transport to use to send this message type.
-Can be overrided by any "messages_transports" records, specifing the transport in the homonym field.';
