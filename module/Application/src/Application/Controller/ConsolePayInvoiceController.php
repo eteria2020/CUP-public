66f2787b0b2ac2e8c00c3507fd401b3d8f63117c
Enrico Taddei <cup@sharengo.eu>
2017-03-30 17:21:17 +0200

    core-170330-wrong-8-days:update

--- module/Application/src/Application/Controller/ConsolePayInvoiceController.php
+++ module/Application/src/Application/Controller/ConsolePayInvoiceController.php
@@ -168,7 +168,7 @@ class ConsolePayInvoiceController extends AbstractActionController
     private function reProcessWrongPayments()
     {
         $this->logger->log(date_create()->format('y-m-d H:i:s').";INF;reProcessWrongPayments;start\n");
-        $tripPaymentsWrong = $this->tripPaymentsService->getTripPaymentsWrong(null, '-30 hours');  //TODO only dev put -2 days
+        $tripPaymentsWrong = $this->tripPaymentsService->getTripPaymentsWrong(null, '-48 hours');  //TODO only dev put -2 days
         $this->logger->log(date_create()->format('H:i:s').";INF;reProcessWrongPayments;count(tripPaymentsWrong);" . count($tripPaymentsWrong) . "\n");
 
         $this->processPaymentsService->processPayments(
