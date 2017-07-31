e4580a1482163cee972be9b6a15e0a3dfa29687b
Enrico Taddei <cup@sharengo.eu>
2017-07-31 09:17:35 +0200

    multi-trip-payment-170728-modal: update 09:17

--- module/Application/src/Application/Controller/UserAreaController.php
+++ module/Application/src/Application/Controller/UserAreaController.php
@@ -189,7 +189,7 @@ class UserAreaController extends AbstractActionController
      */
     public function indexAction()
     {
-        $customerForDebuging = array(2, 3, 4, 22577, 19065, 2981, 39096, 63272, 37841,  28759);
+        $customerForDebuging = array(2, 3, 4, 22577, 19065, 2981, 39096, 63272, 37841);
 
         // check wether the customer still needs to register a credit card
         $customer = $this->userService->getIdentity();
