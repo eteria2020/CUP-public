dbff726eca72282bc01c78517b923b3511e4bed5
Lorenzo Mancini <lorenzo.mancini@sharengo.eu>
2017-08-31 15:57:47 +0200

    insert welcome package logic

--- module/Application/src/Application/Controller/AdditionalServicesController.php
+++ module/Application/src/Application/Controller/AdditionalServicesController.php
@@ -10,6 +10,7 @@ use SharengoCore\Exception\BonusAssignmentException;
 use SharengoCore\Exception\NotAValidCodeException;
 use SharengoCore\Exception\CodeAlreadyUsedException;
 use SharengoCore\Service\BonusService;
+use SharengoCore\Service\TripsService;
 use SharengoCore\Service\CarrefourService;
 use SharengoCore\Service\CustomersBonusPackagesService;
 use SharengoCore\Service\CustomersService;
@@ -58,10 +59,16 @@ class AdditionalServicesController extends AbstractActionController
      * @var AuthenticationService
      */
     private $authService;
+
     /**
      * @var BonusService
      */
     private $bonusService;
+
+    /**
+     * @var TripsService
+     */
+    private $tripsService;
 
     /**
      * @param CustomersService $customerService
@@ -71,6 +78,7 @@ class AdditionalServicesController extends AbstractActionController
      * @param CustomersBonusPackagesService $customersBonusPackagesService
      * @param AuthenticationService $authService
      * @param BonusService $bonusService
+     * @param TripsService $tripsService
      */
     public function __construct(
         CustomersService $customersService,
@@ -80,7 +88,8 @@ class AdditionalServicesController extends AbstractActionController
         PromoCodesOnceService $promoCodeOnceService,
         CustomersBonusPackagesService $customersBonusPackagesService,
         AuthenticationService $authService,
-        BonusService $bonusService
+        BonusService $bonusService,
+        TripsService $tripsService
     ) {
         $this->customersService = $customersService;
         $this->carrefourService = $carrefourService;
@@ -90,6 +99,7 @@ class AdditionalServicesController extends AbstractActionController
         $this->customersBonusPackagesService = $customersBonusPackagesService;
         $this->authService = $authService;
         $this->bonusService = $bonusService;
+        $this->tripsService = $tripsService;
     }
 
     public function additionalServicesAction()
@@ -149,14 +159,25 @@ class AdditionalServicesController extends AbstractActionController
         
         $bonusPackages = $this->customersBonusPackagesService->getAvailableBonusPackges();
         $customer = $this->authService->getIdentity();
-        $verify = count($this->bonusService->verifyWomenBonus($customer));
-        
+        $verifyWomenVoucher = count($this->bonusService->verifyWomenBonusPackage($customer));
+
+        //TODO: verifica primo pagamento e che la prima corsa sia stata pagata // contare numero di pacchetti benvenuto acquistati max 1 per sempre(?)
+        $verifyWelcomePackage = count($this->bonusService->verifyWelcomeBonusPackage($customer));
+        $firstPayment = $customer->getFirstPaymentCompleted();
+        $firstTrip = count($this->tripsService->getFirstTripInvoicedByCustomer($customer));
+
+        $showWelcomePackage = false;
+        if($verifyWelcomePackage == 0 && $firstPayment && $firstTrip == 1){
+            $showWelcomePackage = true;
+        }
+
         return new ViewModel([
             'promoCodeForm' => $form,
             'bonusPackages' => $bonusPackages,
             'customer' => $customer,
-            'verify' => $verify,
-            'mobile' => $mobile
+            'verifyWomenVoucher' => $verifyWomenVoucher,
+            'mobile' => $mobile,
+            'showWelcomePackage' => $showWelcomePackage,
         ]);
     }
 
