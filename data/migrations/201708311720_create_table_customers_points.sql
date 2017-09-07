05aa39f8c14f08a3a73aa686fad2cf37c7efa1de
Enrico Taddei <cup@sharengo.eu>
2017-09-07 16:26:21 +0200

    userArea-170821-points: modify migrations for alter table add_bonus and customers_points with remove column

--- data/migrations/201708311720_create_table_customers_points.sql
+++ data/migrations/201708311720_create_table_customers_points.sql
@@ -8,16 +8,10 @@ CREATE TABLE customers_points (
     total integer NOT NULL,
     residual integer NOT NULL,
     type character varying(100) NOT NULL,
-    operator character varying(100) DEFAULT NULL::character varying,
     valid_from timestamp(0) without time zone DEFAULT NULL::timestamp without time zone,
     duration_days integer,
     valid_to timestamp(0) without time zone DEFAULT NULL::timestamp without time zone,
     description text,
-    promocode_id integer,
-    package_id integer,
-    transaction_id integer,
-    invoice_id integer,
-    invoiced_at timestamp(0) without time zone DEFAULT NULL::timestamp without time zone
 );
 
 
@@ -30,19 +24,7 @@ ADD CONSTRAINT customers_points_pkey PRIMARY KEY (id);
 --ADD FOREIGN KEY to customers_points REFERENCES customers(id)
 ALTER TABLE ONLY customers_points
 ADD CONSTRAINT customer_id FOREIGN KEY (customer_id) REFERENCES customers(id)
-	
---ADD FOREIGN KEY to customers_points REFERENCES invoices(id)
-ALTER TABLE ONLY customers_points
-ADD CONSTRAINT invoice_id FOREIGN KEY (invoice_id) REFERENCES invoices(id);
-	
---ADD FOREIGN KEY to customers_points REFERENCES transactions(id)
-ALTER TABLE ONLY customers_points
-ADD CONSTRAINT transaction_id FOREIGN KEY (transaction_id) REFERENCES transactions(id);
 
 --ADD FOREIGN KEY to customers_points REFERENCES webuser(id)
 ALTER TABLE ONLY customers_points
 ADD CONSTRAINT webuser_id FOREIGN KEY (webuser_id) REFERENCES webuser(id);
-	
---ADD FOREIGN KEY to customers_points REFERENCES promo_codes(id)
-ALTER TABLE ONLY customers_points
-ADD CONSTRAINT promo_code_id FOREIGN KEY (promocode_id) REFERENCES promo_codes(id);
