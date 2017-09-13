5a46aabe14eda90bbd3a5d3535b8714ef429ff4a
Enrico Taddei <cup@sharengo.eu>
2017-09-13 09:53:27 +0200

    cron-170913-update: update

--- scripts/cron/invoice_extra.sh
+++ scripts/cron/invoice_extra.sh
@@ -2,6 +2,6 @@
 #
 
 ts=$(date +'%D %T')
-echo "$ts ------------------" >> /srv/apps/sharengo-publicsite/data/invoice_extra.log
+echo "$ts ------------------" >> /srv/apps/sharengo-publicsite/data/log/invoice_extra.log
 php /srv/apps/sharengo-publicsite/public/index.php generate extra invoices >> /srv/apps/sharengo-publicsite/data/log/invoice_extra.log
 php /srv/apps/sharengo-publicsite/public/index.php generate package invoices >> /srv/apps/sharengo-publicsite/data/log/invoice_package.log
--- scripts/cron/pay_invoice.sh
+++ scripts/cron/pay_invoice.sh
@@ -4,10 +4,14 @@
 #su - postgres -c 'pg_dump -T events -T logs sharengo > sharengo_dump_$(date +"%Y-%m-%d").sql'
 
 ts=$(date +'%D %T')
-echo "$ts ------------------" >> /srv/apps/sharengo-publicsite/data/pay_invoice.log
+echo "$ts ------------------" >> /srv/apps/sharengo-publicsite/data/log/pay_invoice.log
 php /srv/apps/sharengo-publicsite/public/index.php generate locations
 php /srv/apps/sharengo-publicsite/public/index.php pay invoice | tee -a /srv/apps/sharengo-publicsite/data/log/pay_invoice.log
 
 ts=$(date +'%D %T')
-echo "$ts ------------------" >> /srv/apps/sharengo-publicsite/data/business_pay_invoice.log
+echo "$ts ------------------" >> /srv/apps/sharengo-publicsite/data/log/pay_invoice_wrong.log
+php /srv/apps/sharengo-publicsite/public/index.php retry wrong payments | tee -a /srv/apps/sharengo-publicsite/data/log/pay_invoice_wrong.log
+
+ts=$(date +'%D %T')
+echo "$ts ------------------" >> /srv/apps/sharengo-publicsite/data/log/business_pay_invoice.log
 php /srv/apps/sharengo-publicsite/public/index.php business pay invoice >> /srv/apps/sharengo-publicsite/data/log/business_pay_invoice.log
