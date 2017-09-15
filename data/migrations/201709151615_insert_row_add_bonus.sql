af505c1d8eaf21b40428a4f0d02085e9ea46c677
Enrico Taddei <cup@sharengo.eu>
2017-09-15 16:25:38 +0200

    userArea-170915-addRowToAddbonus: add row Aggiunta punti geenerica

--- data/migrations/201709151615_insert_row_add_bonus.sql
+++ data/migrations/201709151615_insert_row_add_bonus.sql
@@ -1,2 +1,2 @@
-INSERT INTO add_bonus (description, type)
-VALUES ('Aggiunta generica di punti', 'point');
+INSERT INTO add_bonus (id, description, type)
+VALUES ((select max(id)+1 from add_bonus), 'Aggiunta generica di punti', 'point');
