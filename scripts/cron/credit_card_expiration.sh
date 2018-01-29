41de9019ee1f7b7e6c8b0430ce386005727ca04e
Lorenzo Mancini <lorenzo.mancini@sharengo.eu>
2018-01-29 14:50:44 +0100

    expired credit card script

--- scripts/cron/credit_card_expiration.sh
+++ scripts/cron/credit_card_expiration.sh
@@ -3,5 +3,5 @@
 #this script alert user that your credit card in in the following month will be disabled
 php /srv/apps/sharengo-publicsite/public/index.php aletr credit card expiration  >> /srv/apps/sharengo-publicsite/data/log/alert_credit_card_expiration.log
 
-#thi script disable the contract and user if your crfedit card is expiration
+#this script disable the contract and user if your crfedit card is expiration
 php /srv/apps/sharengo-publicsite/public/index.php disable credit card  >> /srv/apps/sharengo-publicsite/data/log/disable_credit_card.log
