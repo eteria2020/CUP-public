d93b48c5f1cdc5eac7510d088cbd01140b9fddee
Enrico Taddei <enrico.taddei@sharengo.eu>
2018-02-14 14:12:49 +0100

    user-area-180213-captcha: update 14:12

--- module/Application/src/Application/Form/PromoCodeForm.php
+++ module/Application/src/Application/Form/PromoCodeForm.php
@@ -3,14 +3,29 @@
 namespace Application\Form;
 
 use Zend\Form\Form;
+use Zend\Captcha;
 
-class PromoCodeForm extends Form
-{
+class PromoCodeForm extends Form {
+
     public function __construct(PromoCodeFieldset $promoCodeFieldset) {
 
         parent::__construct('promo-form');
         $this->setAttribute('method', 'post');
 
+        $captcha = new \Zend\Form\Element\Captcha('captcha');
+        $captcha
+                ->setCaptcha(new Captcha\Image(array(
+                    'name' => 'foo',
+                    'wordLen' => 6,
+                    'timeout' => 300,
+                    'dotNoiseLevel' => 40,
+                    'lineNoiseLevel' => 3,
+                    'font' => './public/fonts/arial.ttf',
+                    'imgDir' => './public/captcha',
+                    'imgUrl' => '/captcha/')));
+
         $this->add($promoCodeFieldset);
+        $this->add($captcha);
     }
+
 }
