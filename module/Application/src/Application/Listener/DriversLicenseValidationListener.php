ce95ec6b6eff4aee0010c29ae499d52043225cb0
Enrico Taddei <tommaso.garavaglia@cdesign-group.com>
2018-02-26 10:47:17 +0100

    driverlicensevalidationfix-180223-csd1928: change province when send data to motorizzazione

--- module/Application/src/Application/Listener/DriversLicenseValidationListener.php
+++ module/Application/src/Application/Listener/DriversLicenseValidationListener.php
@@ -72,13 +72,39 @@ final class DriversLicenseValidationListener implements SharedListenerAggregateI
         $data = $e->getParams();
 
         $customer = $this->customersService->findByEmail($data['email'])[0];
-
+        
         // we do not request the validation of the drivers license to the
         // motorizzazione civile is the customer has a foreign drivers license
         if (!$this->customersService->customerNeedsToAcceptDriversLicenseForm($customer)) {
             $data['birthCountryMCTC'] = $this->countriesService->getMctcCode($data['birthCountry']);
-
+            var_dump($data);
+            $data['birthProvince'] = $this->changeProvinceForValidationDriverLicense($data);            
+            var_dump($data);
             $this->enqueueValidationService->validateDriversLicense($data);
         }
     }
+    
+    /*This method returns a different birthPrertince,
+     * so if birthProvince == 'MB' changes to 'MI'
+     * because after creating an MB province the whole city was under the MI province.
+     * While birthProvince == 'LC' and the city is in array$municipalities_lecco_special
+     * sets birthProvince = 'BG' because the city in $municipalities_lecco_special
+     * was under the province of BG, all the More cities were under the province of CO
+     */
+    private function changeProvinceForValidationDriverLicense($data) {
+        switch ($data['birthProvince']) {
+            case 'MB':
+                $birthProvince = 'MI';
+                break;
+            case 'LC':
+                $municipalities_lecco_special = array("CALOLZIOCORTE", "CARENNO", "ERVE", "MONTE MARENZO", "VERCURAGO");
+                if (in_array($data['birthTown'], $municipalities_lecco_special))
+                    $birthProvince = 'BG';
+                else
+                    $birthProvince = 'CO';
+                break;
+        }
+        return $birthProvince;
+    }
+
 }
