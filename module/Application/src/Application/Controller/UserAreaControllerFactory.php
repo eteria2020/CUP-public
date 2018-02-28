3b5a530d293c1324b4b34abb647744ddc64afd51
Enrico Taddei <enrico.taddei@sharengo.eu>
2018-02-28 09:42:16 +0100

    rates-180228-remove-scontatore: update 09:42

--- module/Application/src/Application/Controller/UserAreaControllerFactory.php
+++ module/Application/src/Application/Controller/UserAreaControllerFactory.php
@@ -6,10 +6,9 @@ use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\Stdlib\Hydrator\Reflection;
 
-class UserAreaControllerFactory implements FactoryInterface
-{
-    public function createService(ServiceLocatorInterface $serviceLocator)
-    {
+class UserAreaControllerFactory implements FactoryInterface {
+
+    public function createService(ServiceLocatorInterface $serviceLocator) {
         $sharedLocator = $serviceLocator->getServiceLocator();
 
         $I_customerService = $sharedLocator->get('SharengoCore\Service\CustomersService');
@@ -25,29 +24,13 @@ class UserAreaControllerFactory implements FactoryInterface
         $tripPaymentsService = $sharedLocator->get('SharengoCore\Service\TripPaymentsService');
         $cartasiContractsService = $sharedLocator->get('Cartasi\Service\CartasiContracts');
         $bannerJsonpUrl = $sharedLocator->get('Configuration')['banner-jsonp'];
-        $discounterUrl = $sharedLocator->get('Configuration')['discounterSite']['url'];
         $disableContractService = $sharedLocator->get('SharengoCore\Service\DisableContractService');
         $paymentScriptRunService = $sharedLocator->get('SharengoCore\Service\PaymentScriptRunsService');
         $paymentService = $sharedLocator->get('SharengoCore\Service\PaymentsService');
-        
+
         return new UserAreaController(
-            $I_customerService,
-            $I_tripService,
-            $userService,
-            $invoicesService,
-            $profileForm,
-            $passwordForm,
-            $mobileForm,
-            $driverLicenseForm,
-            $hydrator,
-            $cartasiPaymentsService,
-            $tripPaymentsService,
-            $cartasiContractsService,
-            $bannerJsonpUrl,
-            $discounterUrl,
-            $disableContractService,
-            $paymentScriptRunService,
-            $paymentService
+                $I_customerService, $I_tripService, $userService, $invoicesService, $profileForm, $passwordForm, $mobileForm, $driverLicenseForm, $hydrator, $cartasiPaymentsService, $tripPaymentsService, $cartasiContractsService, $bannerJsonpUrl, $disableContractService, $paymentScriptRunService, $paymentService
         );
     }
+
 }
