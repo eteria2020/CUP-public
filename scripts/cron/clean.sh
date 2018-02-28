dc8d9e69dcedfa11519181bbabe0b4e21fa47338
Enrico Taddei <enrico.taddei@sharengo.eu>
2018-02-28 17:34:04 +0100

    user-area-180213-captcha: update 17:34

--- scripts/cron/clean.sh
+++ scripts/cron/clean.sh
@@ -5,6 +5,7 @@ sudo rm /var/log/redis.log
 sudo find /var/log/apache2/     -type f -name "*.gz"   -mtime +130 -delete
 sudo find /var/log/     -type f -name "*.gz"   -mtime +130 -delete
 sudo find /var/www/api/crashes/ -type f -name "*.json" -mtime +10 -exec rm {} \;
+sudo find /srv/apps/sharengo-publicsite/public/cache -type f -name "*.png" -mtime +1 -delete
 
 echo "Mongo db: new log"
 pid_of_mongo=$(pidof mongod)
