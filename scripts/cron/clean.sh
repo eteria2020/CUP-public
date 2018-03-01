aea0477119ed575b9d5af9deff118ca99da6bdbe
Enrico Taddei <enrico.taddei@sharengo.eu>
2018-03-01 12:14:47 +0100

    user-area-180213-captcha: update 12:14

--- scripts/cron/clean.sh
+++ scripts/cron/clean.sh
@@ -3,9 +3,10 @@
 sudo rm /var/log/redis.log
 
 sudo find /var/log/apache2/     -type f -name "*.gz"   -mtime +130 -delete
-sudo find /var/log/     -type f -name "*.gz"   -mtime +130 -delete
-sudo find /var/www/api/crashes/ -type f -name "*.json" -mtime +10 -exec rm {} \;
-sudo find /srv/apps/sharengo-publicsite/public/cache -type f -name "*.png" -mtime +1 -delete
+sudo find /var/log/             -type f -name "*.gz"   -mtime +130 -delete
+sudo find /var/www/api/crashes/ -type f -name "*.json" -mtime +10  -exec rm {} \;
+sudo find /srv/apps/sharengo-publicsite/public/cache -type f -name "*.png" -mtime +1  -delete
+sudo find /srv/apps/sharengo-admin/public/cache      -type f -name "*.*"   -mtime +30 -delete
 
 echo "Mongo db: new log"
 pid_of_mongo=$(pidof mongod)
@@ -19,5 +20,6 @@ mongo_log_path="/var/log/mongodb/"
 echo "rm -rf "$mongo_log_path"mongodb.log."$timestamp"T*"
 sudo rm -rf "$mongo_log_path"mongodb.log."$timestamp"T*
 
-sudo rm /srv/apps/sharengo-publicsite/data/log/queue-system-dlv.log
+echo "Clear queue of Driver License Validation log"
+sudo rm    /srv/apps/sharengo-publicsite/data/log/queue-system-dlv.log
 sudo touch /srv/apps/sharengo-publicsite/data/log/queue-system-dlv.log
