eb3879ae3fb1f9f6c354c66c051e19403ea42db0
Lorenzo Mancini <lorenzo.mancini@sharengo.eu>
2018-04-09 16:47:08 +0200

    cartasi: update number of retries

--- config/autoload/cartasi.global.php
+++ config/autoload/cartasi.global.php
@@ -11,7 +11,7 @@ return [
         'recurring_payment_url' => 'https://ecommerce.cartasi.it/ecomm/ecomm/ServletS2S',
         'recurring_payment_description' => '',
         'refunding_payment_url' => 'https://ecommerce.cartasi.it/ecomm/ecomm/XPayBo',
-        'number_of_retries' => 2,
+        'number_of_retries' => 1,
         'pause_between_retries' => 1,
         'valid_ip' => '127.0.0.1;185.58.119.117'
     ]
