4f361d0b23121b57c45de5a4363db8d73cbf3380
Enrico Taddei <enrico.taddei@sharengo.eu>
2018-04-30 17:34:51 +0200

    nivea-180430-disable-bonus: update 17:34

--- scripts/cron/bonus.sh
+++ scripts/cron/bonus.sh
@@ -2,7 +2,7 @@
 
 #####################################################################################################################################################
 #BONUS NIVEA
-php /srv/apps/sharengo-publicsite/public/index.php bonus nivea  >> /srv/apps/sharengo-publicsite/data/log/bonus_nivea.log
+#php /srv/apps/sharengo-publicsite/public/index.php bonus nivea  >> /srv/apps/sharengo-publicsite/data/log/bonus_nivea.log
 #####################################################################################################################################################
 
 
