a4640b7890e80a1405d592a5466cbe94c8bc6e35
Enrico Taddei <enrico.taddei@sharengo.eu>
2018-05-08 14:35:58 +0200

    csd-2374-180503-discount-14: upadte 14:35

--- scripts/cron/discounts.sh
+++ scripts/cron/discounts.sh
@@ -3,10 +3,10 @@
 
 ts=$(date +'%D %T')
 echo "$ts ------------------" >> /srv/apps/sharengo-publicsite/data/log/discounts_notify.log
-php /srv/apps/sharengo-publicsite/public/index.php notify disable discount  | tee -a /srv/apps/sharengo-publicsite/data/log/discounts_notify.log
+php /srv/apps/sharengo-publicsite/public/index.php notify disable discount  | tee -a /srv/apps/sharengo-publicsite/data/log/discounts.log
 
 #ts=$(date +'%D %T')
 #echo "$ts ------------------" >> /srv/apps/sharengo-publicsite/data/log/discounts_disable.log
-#php /srv/apps/sharengo-publicsite/public/index.php disable old discounts | tee -a /srv/apps/sharengo-publicsite/data/log/discounts_disable.log
+#php /srv/apps/sharengo-publicsite/public/index.php disable old discounts | tee -a /srv/apps/sharengo-publicsite/data/log/discounts.log
 
-php /srv/apps/sharengo-publicsite/public/index.php renew old discounts | tee -a /srv/apps/sharengo-publicsite/data/log/discounts_renew.log
+php /srv/apps/sharengo-publicsite/public/index.php renew old discounts | tee -a /srv/apps/sharengo-publicsite/data/log/discounts.log
