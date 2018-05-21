b64c77844337dce2a5bf73105896bf10f1522609
Enrico Taddei <enrico.taddei@sharengo.eu>
2018-05-21 15:08:13 +0200

    csd-2512-email-renew-discount: update 15:08

--- scripts/cron/discounts.sh
+++ scripts/cron/discounts.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 
-ts=$(date +'%D %T')
-echo "$ts ------------------" >> /srv/apps/sharengo-publicsite/data/log/discounts_notify.log
-php /srv/apps/sharengo-publicsite/public/index.php notify disable discount  | tee -a /srv/apps/sharengo-publicsite/data/log/discounts.log
+#ts=$(date +'%D %T')
+#echo "$ts ------------------" >> /srv/apps/sharengo-publicsite/data/log/discounts_notify.log
+#php /srv/apps/sharengo-publicsite/public/index.php notify disable discount  | tee -a /srv/apps/sharengo-publicsite/data/log/discounts.log
 
 #ts=$(date +'%D %T')
 #echo "$ts ------------------" >> /srv/apps/sharengo-publicsite/data/log/discounts_disable.log
