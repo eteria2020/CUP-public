7346549641bc80877aff554a2b3e6dd2d7178e1c
Enrico Taddei <enrico.taddei@sharengo.eu>
2018-06-08 09:24:16 +0200

    signup1.5-MGM: update 09:24

--- data/migrations/201805291805_update_member_get_member.sql
+++ data/migrations/201805291805_update_member_get_member.sql
@@ -6,7 +6,7 @@ INSERT INTO promo_codes (id, promocodesinfo_id, promocode, description, active) 
 
 INSERT INTO mails (id, subject,content,enable,language, category) VALUES (
 nextval('mails_id_seq'),
-'Share''ngo: Codice amico',
+'Share''ngo: Invita un amico',
 '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
