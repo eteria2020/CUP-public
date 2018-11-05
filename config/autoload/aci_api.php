5e756593fe6b0cad5f94460625d0e43130cbbcb8
Lorenzo Mancini <lorenzo.mancini@sharengo.eu>
2018-11-05 09:48:18 +0100

    initial ACI

---
+++ config/autoload/aci_api.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'aci' => [
+        'auth_url' => 'http://col-www.aci.it/uyc/v3/auth/login?p=',
+        'username' => 'sociocollaudo',
+        'password' => 'sociocollaudo',
+        'verify_url' => 'http://col-www.aci.it/uyc/v3/discounts/',
+        'id_esercizio' => 'M6WP1VEW1',
+    ]
+];
