4cd91b12a3eade105b8e8a5102099a964c84fc4e
Lorenzo Mancini <lorenzo.mancini@sharengo.eu>
2019-03-01 10:22:36 +0100

    update silverlist script

--- scripts/cron/silver_list.sh
+++ scripts/cron/silver_list.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 #call the function that add minutes to silver members
 
-PSQL='psql -p 5433'
+PSQL='psql -p 5433 -c '
 
-$PSQL <<THE_END
-SELECT "addBonusSilverList"();
-THE_END
+$PSQL  "SELECT \"addBonusSilverList\"();"
