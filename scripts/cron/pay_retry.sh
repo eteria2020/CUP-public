e14cd720f8d690ba9fa412422d23f058c1b93cb2
Enrico Taddei <enrico.taddei@sharengo.eu>
2019-03-26 17:36:23 +0100

    nexi-190326-block-190331

--- scripts/cron/pay_retry.sh
+++ scripts/cron/pay_retry.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 #
 
+now=$(date +%s)
+no_run_init=$(date -d "2019-03-31 00:00:00" +%s)
+no_run_end=$(date -d "2019-03-31 07:30:00" +%s)
+
+# block the run if now>="2019-03-31 00:00:00" and now<="2019-03-31 07:30:00"
+if [ $now -ge $no_run_init ] && [ $now -le $no_run_end ]; then
+    ts=$(date +'%D %T')
+    echo "$ts ----- NO RUN ---------" >> /var/log/sharengo-publicsite/data/log/pay_invoice_wrong.log
+    exit 1
+fi
+
 ts=$(date +'%D %T')
 echo "$ts ------------------" >> /var/log/sharengo-publicsite/data/log/pay_invoice_wrong.log
 php /srv/apps/sharengo-publicsite/public/index.php retry wrong payments | tee -a /var/log/sharengo-publicsite/data/log/pay_invoice_wrong.log
