32226ae7fae578463b0149da88ac28fa17994968
Enrico Taddei <enrico.taddei@sharengo.eu>
2019-05-28 15:15:28 +0200

    telepass-190527-update-public: update 15:15

--- data/migrations/201905271127_partner_telepass_update.sql
+++ data/migrations/201905271127_partner_telepass_update.sql
@@ -1,2 +1,2 @@
-UPDATE partners SET params='{"signup":{"validIp":""},"payments":{"uri":"http://api-dev.urbi.co","authorization":"sharengo_test_key"},"notifyCustomerStatus":{"uri":""},"importInvoice":{"uri":""},"exportRegistries":{"path":""}}' WHERE code='telepass';
+UPDATE partners SET params='{"signup":{"validIp":""},"payments":{"uri":"https://api-dev.urbi.co","authorization":"sharengo_test_key"},"notifyCustomerStatus":{"uri":""},"importInvoice":{"uri":""},"exportRegistries":{"path":""}}' WHERE code='telepass';
 
