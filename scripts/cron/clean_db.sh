84c12e9f6197fbfba2942da6bb34d8ed24687c9e
Lorenzo Mancini <lorenzo.mancini@sharengo.eu>
2019-10-28 17:00:45 +0100

    The script has been completed.
    The tables that will be cleaned are:
    
    TABELLA                                           DATE <
    
    commands                                          3 months
    reservations_archive                              3 months
    customer_locations                               10 months
    messages_outbox                                   3 months
    customer_deactivations                            3 years
    trip_bonuses                                      3 years
    subscription_payments                             3 years
    subscription_paymentscartasi_csv_anomalies_notes  3 years
    cartasi_csv_anomalies                             3 years
    bonus_package_payments                            3 years
    trip_payment_tries_canceled                       3 years
    extra_payment_tries_canceled                      3 years
    extra_payment_tries                               3 years
    safo_penalty                                      3 years
    extra_payments_canceled                           3 years
    extra_payments                                    3 years
    customers_bonus                                   3 years
    trip_payment_tries                                3 years
    transactions                                      3 years
    contracts                                         3 years

--- scripts/cron/clean_db.sh
+++ scripts/cron/clean_db.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 #clean old commands and old archive reservations
-
+#
 time='3 months'
-
-PSQL='psql -p 5433'
+#
+years='3 years'
+#
+now="$(date --date="3 year ago" "+%Y-%m-%d 00:00:00")"
+#
+echo $now
+#
+PSQL='psql postgresql://localhost:5432/sharengo?sslmode=require -U sharengo'
+#
+#PSQL='psql -p 5433'
 
 $PSQL <<THE_END
 DELETE FROM commands
@@ -14,4 +22,100 @@ DELETE FROM customer_locations
 WHERE timestamp < (now() - interval '10 month');
 DELETE FROM messages_outbox
 WHERE submitted < (now() - interval'$time');
+
+
+DELETE FROM public.customer_deactivations where inserted_ts < '$now';
+
+DELETE FROM public.trip_bonuses where timestamp_end < '$now';
+
+
+BEGIN;
+DELETE FROM public.subscription_payments
+WHERE public.subscription_payments.transaction_id IN
+      ( select t.id from public.transactions t
+			left join public.contracts c on c.id = t.contract_id
+			where c.inserted_ts < '$now'
+      );
+DELETE FROM public.cartasi_csv_anomalies_notes
+WHERE public.cartasi_csv_anomalies_notes.cartasi_csv_anomaly_id IN
+      ( SELECT cc.id 
+	    FROM public.cartasi_csv_anomalies cc
+	    	left join public.transactions t on t.id = cc.transaction_id
+	   		left join public.contracts c on c.id = t.contract_id
+	    	where c.inserted_ts < '$now'
+      );
+DELETE FROM public.cartasi_csv_anomalies
+WHERE public.cartasi_csv_anomalies.transaction_id IN
+      ( select t.id from public.transactions t
+			left join public.contracts c on c.id = t.contract_id
+			where c.inserted_ts < '$now'
+      );
+DELETE FROM public.bonus_package_payments
+WHERE public.bonus_package_payments.transaction_id IN
+      ( select t.id from public.transactions t
+			left join public.contracts c on c.id = t.contract_id
+			where c.inserted_ts < '$now'
+      );
+DELETE FROM public.trip_payment_tries_canceled
+WHERE public.trip_payment_tries_canceled.transaction_id IN
+      ( select t.id from public.transactions t
+			left join public.contracts c on c.id = t.contract_id
+			where c.inserted_ts < '$now'
+      );
+DELETE FROM public.extra_payment_tries_canceled
+WHERE public.extra_payment_tries_canceled.extra_payment_canceled_id IN
+      ( SELECT e.id 
+	    	FROM public.extra_payments e
+	    	left join public.transactions t on t.id = e.transaction_id
+	   		left join public.contracts c on c.id = t.contract_id
+	    	where c.inserted_ts < '$now'
+      );
+DELETE FROM public.extra_payment_tries
+WHERE public.extra_payment_tries.extra_payment_id IN
+      ( SELECT e.id 
+	    	FROM public.extra_payments e
+	    	left join public.transactions t on t.id = e.transaction_id
+	   		left join public.contracts c on c.id = t.contract_id
+	    	where c.inserted_ts < '$now'
+      );
+DELETE FROM public.safo_penalty
+WHERE public.safo_penalty.extra_payment_id IN
+      ( SELECT e.id 
+	    	FROM public.extra_payments e
+	    	left join public.transactions t on t.id = e.transaction_id
+	   		left join public.contracts c on c.id = t.contract_id
+	    	where c.inserted_ts < '$now'
+      );
+DELETE FROM public.extra_payments_canceled
+WHERE public.extra_payments_canceled.transaction_id IN
+      ( select t.id from public.transactions t
+			left join public.contracts c on c.id = t.contract_id
+			where c.inserted_ts < '$now'
+      );	  
+DELETE FROM public.extra_payments
+WHERE public.extra_payments.transaction_id IN
+      ( select t.id from public.transactions t
+			left join public.contracts c on c.id = t.contract_id
+			where c.inserted_ts < '$now'
+      );
+DELETE FROM public.customers_bonus
+WHERE public.customers_bonus.transaction_id IN
+      ( select t.id from public.transactions t
+			left join public.contracts c on c.id = t.contract_id
+			where c.inserted_ts < '$now'
+      );
+DELETE FROM public.trip_payment_tries
+WHERE public.trip_payment_tries.transaction_id IN
+      ( select t.id from public.transactions t
+			left join public.contracts c on c.id = t.contract_id
+			where c.inserted_ts < '$now'
+      );
+DELETE FROM public.transactions
+WHERE public.transactions.contract_id IN
+      ( SELECT c.id 
+	    	FROM public.contracts c
+	    	WHERE c.inserted_ts < '$now'
+      );
+DELETE FROM public.contracts WHERE public.contracts.inserted_ts < '$now';
+COMMIT;
 THE_END
